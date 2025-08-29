d4af899538df1901548bdcb2e6b3c077dc170ec0
Arthurcmo <arthur_munhoz@hotmail.com>
2025-08-29 09:42:48 -0300

    HelmetContact change

--- src/pages/contact/HelmetContact.jsx
+++ src/pages/contact/HelmetContact.jsx
@@ -6,26 +6,26 @@ const HelmetContact = () => {
     <div>
     <h1 className="visually-hidden">Contato</h1>
     <Helmet>
-      <title>Contato | Sovierzoski | Carleial | Magnabosco Advogados</title>
+      <title>Contato | Sovierzoski, Carleial & Magnabosco advogados</title>
       <meta
         name="description"
-        content="Entre em contato com a equipe da Sovierzoski | Carleial | Magnabosco Advogados. Atendimento ágil, ético e personalizado para você ou sua empresa."
+        content="Entre em contato com a equipe da Sovierzoski, Carleial & Magnabosco advogados. Atendimento ágil, ético e personalizado para você ou sua empresa."
         />
       <meta
         name="keywords"
         content="contato, advogados Curitiba, Sovierzoski Carleial Magnabosco, consultoria jurídica, advocacia empresarial"
       />
-      <meta name="author" content="Sovierzoski | Carleial | Magnabosco Advogados" />
+      <meta name="author" content="Sovierzoski, Carleial & Magnabosco Advogados" />
       <meta property="og:title" content="Contato | Sovierzoski | Carleial | Magnabosco Advogados" />
       <meta property="og:description" content="Fale conosco. Atendimento com excelência e dedicação." />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://www.seusite.com.br/contato" />
-      <meta property="og:image" content="https://www.seusite.com.br/imagens/logo-social.jpg" />
+      <meta property="og:url" content="https://www.https://scmradvogados.com.br.com.br/contato" />
+      <meta property="og:image" content="https://www.https://scmradvogados.com.br.com.br/imagens/logo-social.jpg" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Contato | SCM Advogados" />
+      <meta name="twitter:title" content="Contato |  Advogados" />
       <meta name="twitter:description" content="Fale com nosso time jurídico especializado." />
-      <meta name="twitter:image" content="https://www.seusite.com.br/imagens/logo-social.jpg" />
-      <link rel="icon" href="https://www.seusite.com.br/favicon.png" type="image/png" />
+      <meta name="twitter:image" content="https://www.https://scmradvogados.com.br.com.br/imagens/logo-social.jpg" />
+      <link rel="icon" href="https://www.https://scmradvogados.com.br.com.br/favicon.png" type="image/png" />
     </Helmet>
     </div>
     
